369e9c4c50d44ff28b26b7c6226f988c9562da78
tarikgwa <admin@example.com>
2017-10-08 15:15:11 +0000

    update

---
+++ initdock.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp /home/mybb/docker.yml /etc/ansible 
+cd /etc/ansible ansible-playbook -i "localhost," -c local docker.yml -vvv 
+sudo apt-get -y install python-pip 
+sudo pip install docker-compose 
+cd /home/mybb/docker 
+sudo docker-compose build 
+sudo docker-compose up -d
