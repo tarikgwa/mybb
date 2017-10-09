0acb9183907bee8b40001455ecf430756900b646
tarikgwa <admin@example.com>
2017-10-09 11:38:03 +0000

    update

--- README.md
+++ README.md
@@ -2,6 +2,7 @@ MYBB Forum Application
 
 # mybb version 1.8.12 application dockerized with docker and build/run/ship by docker-compose.
 The latest MyBB release and is a security.
+Provisionning the Server by Ansibe.
 
 *****Installation Instruction*****
 
--- initdock.sh
+++ initdock.sh
@@ -7,6 +7,11 @@ cp /home/mybb/pkg.yml /etc/ansible
 cd /etc/ansible
 ansible-playbook -i "localhost," -c local pkg.yml -vvv
 
+#Change  permissions of application files and directories.
+cd /home/mybb/html
+chmod 666 inc/config.php inc/settings.php inc/languages/english/*.php inc/languages/english/admin/*.php
+chmod 777 cache/ cache/themes/ uploads/ uploads/avatars/ admin/backups/
+
 #Build and ship the mybb application by docker compose
 cd /home/mybb/docker
 docker-compose up -d
